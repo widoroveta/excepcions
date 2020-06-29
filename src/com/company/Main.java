d5d5caaafd219ddfbac9d8a9b32b5750d306771c
widoroveta <55321418+widoroveta@users.noreply.github.com>
2020-06-29 06:24:32 -0300

    version 1.02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,23 +16,15 @@ public class Main {
         Question q2 = new Question("what is the capital of serbia? ", "  Kiev", "  Belgrade", "   London", 2);
         Question q3 = new Question(" who is the father of psychology", "  Einstein", "  Freud", "   Wundt", 2);
         Question q4 = new Question("how much is it 325+294? ", "  619", "  728", "   629", 1);
-        Question q5 = new Question("what is the capital of serbia? ", "  Kiev", "  Belgrade", "   London", 2);
-        Question q6 = new Question("what is the capital of serbia? ", "  Kiev", "  Belgrade", "   London", 2);
-        Question q7 = new Question("what is the capital of serbia? ", "  Kiev", "  Belgrade", "   London", 2);
-        Question q8 = new Question("what is the capital of serbia? ", "  Kiev", "  Belgrade", "   London", 2);
-        Question q9 = new Question("what is the capital of serbia? ", "  Kiev", "  Belgrade", "   London", 2);
-        Question q10 = new Question("what is the capital of serbia? ", "  Kiev", "  Belgrade", "   London", 2);
-        File file = new File("Answer.json");
+     File file = new File("Answer.json");
         ObjectMapper mapper = new ObjectMapper();
         MapQuestion q = new MapQuestion();
         MapQuestion questions = new MapQuestion();
-
         questions.put(1, q1);
         questions.put(2, q2);
         questions.put(3, q3);
         questions.put(4, q4);
         try {
-
             mapper.writeValue(file, questions);
         } catch (IOException e) {
             e.printStackTrace();
@@ -42,18 +34,27 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        Question q5= new Question("who won the 2018 world cup? ","Argentina", "Germany","France",3 );
+        questions.put(5, q5);
+        try {
+            mapper.writeValue(file, questions);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }    try {
+            q = mapper.readValue(file, MapQuestion.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println(q.get(1).toString());
         System.out.println(q.get(1).anwsered(scanner.nextInt()));
-
-
         System.out.println(q.get(2).toString());
         System.out.println(q.get(2).anwsered(scanner.nextInt()));
         System.out.println(q.get(3).toString());
         System.out.println(q.get(3).anwsered(scanner.nextInt()));
         System.out.println(q.get(4).toString());
         System.out.println(q.get(4).anwsered(scanner.nextInt()));
-
+        System.out.println(q.get(5).toString());
+        System.out.println(q.get(5).anwsered(scanner.nextInt()));
         System.out.println(q.Score());
     }
 }
